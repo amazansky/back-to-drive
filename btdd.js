5d0b8fd31a6a01ddc878aec493955288aedbbc5a
RobotXLabs <RobotXLabs@users.noreply.github.com>
2016-03-02 08:58:26 -0500

    Yay!

--- btdd.js
+++ btdd.js
@@ -1,1 +1,3 @@
 document.querySelector("#docs-branding-container a").href = "http://drive.google.com";
+document.querySelector("#docs-branding-container > a").setAttribute("data-tooltip", "Back to Drive");
+document.querySelector("#docs-branding-container > a").setAttribute("aria-label", "Back to Drive");
