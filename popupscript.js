d6ab40aec3f053504333d5098cd8cd48c9b94808
RobotXLabs <RobotXLabs@users.noreply.github.com>
2016-04-25 10:08:41 -0400

    Update popupscript.js

--- popupscript.js
+++ popupscript.js
@@ -1,1 +1,1 @@
-window.open("https://accounts.google.com/ServiceLogin?&continue=https://drive.google.com/%23&followup=https://drive.google.com/",'_blank');
+window.open("http://drive.google.com",'_blank');
