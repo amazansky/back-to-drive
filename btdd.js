727950c340366c25c2e9690c59ba2d025c381e28
RobotXLabs <databasemaster9233@gmail.com>
2016-05-21 08:38:25 -0400

    Get ready for v0.2!
    New logos and more!

--- btdd.js
+++ btdd.js
@@ -1,3 +1,9 @@
+/*!
+ * Back To Drive v0.2.0 (http://robotxlabs.github.io/back-to-drive)
+ * Copyright 2015-2016 RobotXLabs
+ * Licensed under MIT
+ * btdd.js
+ */
 document.querySelector("#docs-branding-container a").href = "http://drive.google.com";
 window.onload = function(){
   document.querySelector("#docs-branding-container a").setAttribute("data-tooltip", "Back to Drive");
--- popupscript.js
+++ popupscript.js
@@ -1,1 +1,7 @@
+/*!
+ * Back To Drive v0.2.0 (http://robotxlabs.github.io/back-to-drive)
+ * Copyright 2015-2016 RobotXLabs
+ * Licensed under MIT
+ * popupscript.js
+ */
 window.open("http://drive.google.com",'_blank');
