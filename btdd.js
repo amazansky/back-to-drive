9aac0df64a3a79e18c5bbff0fe667533f3f1786b
RobotXLabs <databasemaster9233@gmail.com>
2016-05-21 08:18:07 -0400

    Onload

--- btdd.js
+++ btdd.js
@@ -1,3 +1,5 @@
 document.querySelector("#docs-branding-container a").href = "http://drive.google.com";
-document.querySelector("#docs-branding-container a").setAttribute("data-tooltip", "Back to Drive");
-document.querySelector("#docs-branding-container a").setAttribute("aria-label", "Back to Drive");
+window.onload = function(){
+  document.querySelector("#docs-branding-container a").setAttribute("data-tooltip", "Back to Drive");
+  document.querySelector("#docs-branding-container a").setAttribute("aria-label", "Back to Drive");
+};
